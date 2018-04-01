e0d14eb086ad4a47c520468b8113fba910c292fa
Rahul Agrahari <agraharisrm@gmail.com>
2018-04-01 15:28:40 +0100

    mo auth

--- HawkEyeNews/trendingNewsModel/views.py
+++ HawkEyeNews/trendingNewsModel/views.py
@@ -8,12 +8,12 @@ from .models import trendingNews
 class trendinNewsApiView(APIView):
 
     def get(self, request):
-        user = request.user
-        if user.is_authenticated:
+        # user = request.user
+        # if user.is_authenticated:
             tNews = trendingNews.objects.all()
             serializer = trendingNewsSerializer(tNews, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response({"status": "authentication failed"}, status=status.HTTP_401_UNAUTHORIZED)
+        # else:
+        #     return Response({"status": "authentication failed"}, status=status.HTTP_401_UNAUTHORIZED)
 
 
